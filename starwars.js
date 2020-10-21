53c69cbec353c4fcaf250ab92d5a1df3708df4bd
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-10-21 15:39:00 -0400

    [js] Add object

---
+++ starwars.js
@@ -0,0 +1,9 @@
+const starwars = {
+    image: 'some path to image',
+    length: '1:00',
+    title: 'Special Look | The Mandalorian | Disney+',
+    username: 'Star Wars',
+    views: '1M views',
+    uploaded: 24,
+}
+console.log(starwars.title)
